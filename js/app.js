01ea395f498966e1f8e142d1725d356c393c44db
carlacentenor <carlacentenor@gmail.com>
2019-10-22 18:40:05 -0500

    add mobile bb

--- js/app.js
+++ js/app.js
@@ -1,13 +1,44 @@
 
         $(document).ready(function () {
-            $('#myContainer').multiscroll({
-                // navigation: true,
-                loopBottom: false,
-                loopTop: false,
-              
-            });
-            
+           
+            if(screen.width<1025){
+
+                   // Counter
+                   const show = document.querySelector('.count'),
+                   number = Number(show.innerHTML) + 1;
+
+               let counter = 5970,
+                   delay = 1,
+                   x = number / 100,
+                   y = 0;
+
+               counter_js();
+
+               function counter_js() {
+                   y++;
+                   delay = (x - y);
+                   show.innerHTML = counter.toString();
+                   counter++;
+                   if (counter < number) {
+                       setTimeout(function () {
+                           counter_js();
+                       }, delay)
+                   }
+               }
+
+
+            }
+            else{
+                $('#myContainer').multiscroll({
+                    // navigation: true,
+                    loopBottom: false,
+                    loopTop: false,
+                  
+                });
+            }
 
         });
 
         
+
+       
