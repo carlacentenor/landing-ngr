690fc3d015fb108222572cabde1c80643b3676ee
carlacentenor <carlacentenor@gmail.com>
2019-10-24 14:55:36 -0500

    add fullpage

--- js/app.js
+++ js/app.js
@@ -2,29 +2,14 @@
         $(document).ready(function () {
            
             if(screen.width<1025){
-
-                   // Counter
-                   const show = document.querySelector('.count'),
-                   number = Number(show.innerHTML) + 1;
-
-               let counter = 5970,
-                   delay = 1,
-                   x = number / 100,
-                   y = 0;
-
-               counter_js();
-
-               function counter_js() {
-                   y++;
-                   delay = (x - y);
-                   show.innerHTML = counter.toString();
-                   counter++;
-                   if (counter < number) {
-                       setTimeout(function () {
-                           counter_js();
-                       }, delay)
-                   }
-               }
+                new fullScroll({
+                    mainElement: 'main',
+                    displayDots: false,
+                    dotsPosition: 'right',
+                    animateTime: 0.7,
+                    animateFunction: 'ease'
+                });
+             
 
 
             }
